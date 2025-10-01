a36822b7c8565fd46833bdf4f158af9c630b1c3d
ITB2526JaumeLloveras <jaume.lloveras.7e9@itb.cat>
2025-10-01 09:02:55 +0200

    Tasca1

--- Edat.py
+++ Edat.py
@@ -1,7 +1,13 @@
 edat=int(input("Hola, diga'm la teva edat"))
 if edat>=18:
     print("Molt bé, ets major d'edat")
+    edatMJ=(edat)-18
+    if edatMJ==0:
+        print("Has complert aquest any, enorabona")
+    else:
+        print(f"Portes sent major d'edat {edatMJ}")
     print("programa acabat")
 else:
     edatfaltant=18-(edat)
     print(f"Encara ets menor, et falten {edatfaltant} ")
+    print("Programa finalitzat")
